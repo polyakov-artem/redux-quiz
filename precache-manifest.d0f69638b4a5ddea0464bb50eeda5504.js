0568355fa4ad3105dbdd5462153677e0151838f7
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2019-07-31 20:41:12 +0400

    Updates

---
+++ precache-manifest.d0f69638b4a5ddea0464bb50eeda5504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5adf96cd308c9e6bad5",
+    "url": "/redux-quiz/static/js/runtime~main.b25d27aa.js"
+  },
+  {
+    "revision": "f7268637bc843817ef92",
+    "url": "/redux-quiz/static/js/main.62e37bb7.chunk.js"
+  },
+  {
+    "revision": "3884e5de139388223c82",
+    "url": "/redux-quiz/static/js/2.3cd5f6e9.chunk.js"
+  },
+  {
+    "revision": "f7268637bc843817ef92",
+    "url": "/redux-quiz/static/css/main.d1e13e3b.chunk.css"
+  },
+  {
+    "revision": "dcd1e174eef5aa5d90e02de0d2f9592d",
+    "url": "/redux-quiz/index.html"
+  }
+];
